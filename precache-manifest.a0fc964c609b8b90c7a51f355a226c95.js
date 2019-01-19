235dbccc46594c60b8245be135f2a680ddae8686
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-01-19 15:11:12 +0000

    Deploy Doro-nyong/BUMP to github.com/Doro-nyong/BUMP.git:gh-pages

---
+++ precache-manifest.a0fc964c609b8b90c7a51f355a226c95.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "77f672bfa9514df1ed45fb816d82eab2",
+    "url": "/static/media/updown-background.77f672bf.svg"
+  },
+  {
+    "revision": "1d7ff8b01ce16411f222da34219875ff",
+    "url": "/static/media/updown-arrow_up.1d7ff8b0.svg"
+  },
+  {
+    "revision": "09f88222286d8f33374583ddac9cca3a",
+    "url": "/static/media/updown-arrow_down.09f88222.svg"
+  },
+  {
+    "revision": "90a49c488506c70c9839c69329307a9c",
+    "url": "/static/media/logo.90a49c48.svg"
+  },
+  {
+    "revision": "e75f78394140250af0e94ad245425fe7",
+    "url": "/static/media/input_button.e75f7839.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5dd90c71f305affbd655",
+    "url": "/static/js/main.5dd90c71.chunk.js"
+  },
+  {
+    "revision": "65d5e5e1bfbcd5871299",
+    "url": "/static/js/1.65d5e5e1.chunk.js"
+  },
+  {
+    "revision": "fed07678948e89c961252e69272508b9",
+    "url": "/index.html"
+  }
+];
